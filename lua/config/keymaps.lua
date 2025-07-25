f23097a85de66624e78d9f28c3400ebaaf6b4f85
yangyang <charlesmaxwellyoung@gmail.com>
2025-07-25 17:53:01 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("i", "jj", "<ESC>", {
+  desc = "escape insert mode with 'jj'",
+  silent = true,
+})
+
+map("i", "jk", "<cmd>w<cr><esc>", {
+  desc = "escape and save with 'jk'",
+  silent = true,
+})
