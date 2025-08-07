652c054103cc5e950a51463ecba918f4d2c360b1
yangyang <charlesmaxwellyoung@gmail.com>
2025-08-07 11:54:04 +0800

    update config

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-if vim.g.vscode then
-  -- VSCode extension
-else
-  -- ordinary Neovim
+if vim.g.neovide then
+  -- neovide specific settings
+  vim.print(vim.g.neocide_version)
+  vim.g.guifont = "Mapple Momo NF CN:h14"
 end
